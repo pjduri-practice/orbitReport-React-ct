9b0751952124d13962cf6602a1a701bbb67cf928
Your Name <108307350+pjduri@users.noreply.github.com>
2023-12-12 18:55:23 -0600

    added table - buttons filter results

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,26 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
-  );
-};
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Orbit Type</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map(s => <>
+          <tr>
+            <td>{s.name}</td>
+            <td>{s.orbitType}</td>
+            <td>{s.launchDate}</td>
+            <td>{s.operational ? 'Active' : 'Inactive'}</td>
+          </tr>
+        </>)}
+      </tbody>
+    </table>
+  )
+}
 
-export default Table;
+export default Table
